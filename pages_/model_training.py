e4d8a4e1d7a29fae2ae2960abcd8c3b294ad7754
Rufelle Emmanuel Pactol <rufelleemmanuel.pactol@cit.edu>
2024-08-01 20:53:12 +0800

    try

--- pages_/model_training.py
+++ pages_/model_training.py
@@ -171,9 +171,9 @@ def train_nn(df: pd.DataFrame, k=5):
 def discretize_weights(weight):
     if weight < 50:
         return 0
-    if 50 <= weight < 85:
+    if 30 <= weight < 65:
         return 1
-    if 85 <= weight <= 100:
+    if 65 <= weight <= 100:
         return 2
 
 # Note: Ensure that all required dependencies are installed and the Streamlit app is set up correctly.
